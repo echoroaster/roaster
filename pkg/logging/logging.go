62af7de36742d17b31e18eca8af6791d941053ce
Tony <tony.yip@echoroaster.com>
2020-08-03 02:28:26 +0800

    feat(logging): implement better logging level with environment variable

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -11,5 +11,19 @@ var RootLogger = logrus.New()
 func init() {
 	if os.Getenv("DEBUG") != "" {
 		RootLogger.SetLevel(logrus.DebugLevel)
+	} else {
+		switch os.Getenv("LOG_LEVEL") {
+		case "trace":
+			RootLogger.SetLevel(logrus.TraceLevel)
+		case "debug":
+			RootLogger.SetLevel(logrus.DebugLevel)
+		case "info":
+			RootLogger.SetLevel(logrus.InfoLevel)
+		case "warn":
+			RootLogger.SetLevel(logrus.WarnLevel)
+		case "error":
+			RootLogger.SetLevel(logrus.ErrorLevel)
+		default:
+		}
 	}
 }
