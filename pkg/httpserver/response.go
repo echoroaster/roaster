ade4f4e233f4c0e96c9bbb90115ea965a633b064
Tony <tony.yip@echoroaster.com>
2020-08-03 18:28:31 +0800

    feat(httpserver): add data response

--- pkg/httpserver/response.go
+++ pkg/httpserver/response.go
@@ -1,5 +1,11 @@
 package httpserver // import "github.com/echoroaster/roaster/pkg/httpserver"
 
+func DataResponse(value interface{}) map[string]interface{} {
+	return map[string]interface{}{
+		"data": value,
+	}
+}
+
 type ErrorResponse struct {
 	Error ErrorMessage `json:"error"`
 }
